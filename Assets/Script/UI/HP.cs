1b2986c3d952da4baf4485414ec26f3857d5f997
yusheng <2086867082@qq.com>
2023-09-19 18:05:14 +0800

    Feat:添加主角血量，敌人行为逻辑，主角受伤

---
+++ Assets/Script/UI/HP.cs
@@ -0,0 +1,36 @@
+using HutongGames.PlayMaker.Actions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject heartPrefab;
+    public Sprite halfHeart;
+
+    private void Update()
+    {
+        //清空子节点
+        var childCount = transform.childCount;
+        for(int i= childCount -1;i>=0;i--)
+        {
+            var child = transform.GetChild(i);
+            Destroy(child.gameObject);
+
+        }
+
+        //显示血量
+        var hp = player.GetComponent<Player>().hp;
+        for(int i=0;i<hp;i++)
+        {
+            var heart = Object.Instantiate(heartPrefab,transform);
+            if(hp-i<=0.5)
+            {
+                heart.GetComponent<Image>().sprite = halfHeart;
+            }
+
+        }
+    }
+}
